b4a8ced8b6cbd853657b40d5bf9e23733e0b4b8b
AlexTrejo92 <arq.alextrejo@gmail.com>
2023-04-07 14:48:27 -0600

    Further development of code after tutoring session. Addition of another html file to store highscores

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,12 +8,41 @@ var hideCont = document.querySelector('.content');
 
 var timer;
 
-var timerCount = 60;
+var timerCount = 20;
+
+var firstquestion = 'h1';
+
+var questionOptions = 'button';
+
+var headerSelection = document.querySelectorAll("h1")
+
+var btnSelection = document.querySelectorAll('button')
+
+var questionIndex = 0;
+
+var questions = [
+    {
+        questionText: "Question1",
+        options: ["red","blue","green"],
+        correctAnswer: "blue",
+    },
+    {
+        questionText: "Question2",
+        options: ["red","blue","green"],
+        correctAnswer: "blue",
+    },
+    {
+        questionText: "Question3",
+        options: ["red","blue","green"],
+        correctAnswer: "blue",
+    },
+]
 
 function startQuiz() {
-    timerCount = 60;
-    startTimer()
+    timerCount = 20;
+    startTimer();
     hideCont.setAttribute('style', 'display: none');
+    runGame();
 }
 
 function startTimer() {
@@ -28,5 +57,68 @@ timer = setInterval(function() {
     }
 }, 1000);
 }
+// When timer runs out, it should stop the quiz (or runGame function?) and show a sumbit highscore form.
+
+function runGame() {
+    var questionEl = document.getElementById("questionText");
+    console.log(questions[1].questionText);
+    questionEl.textContent = questions[questionIndex].questionText
+
+    var qArray = questions[questionIndex];
+
+for (var i = 0; i < qArray.options.length; i++) {
+var tag = document.createElement(questionOptions);
+tag.textContent = qArray.options[i];
+document.body.appendChild(tag);
+tag.className = 'questionStyle';
+}
+
+    // var tag = document.createElement(firstquestion);
+    // tag.textContent = "This is a Question";
+    // document.body.appendChild(tag);
+    // tag.className = 'questionStyle';
+
+    // var tag = document.createElement(questionOptions);
+    // tag.textContent = "this is a button option 1";
+    // document.body.appendChild(tag);
+    // tag.className = 'questionsBtn';
+
+    // var tag = document.createElement(questionOptions);
+    // tag.textContent = "this is a button option 2";
+    // document.body.appendChild(tag);
+    // tag.className = 'questionsBtn';
+
+    // var tag = document.createElement(questionOptions);
+    // tag.textContent = "this is a button option 3";
+    // document.body.appendChild(tag);
+    // tag.className = 'questionsBtn';
+
+    // var tag = document.createElement(questionOptions);
+    // tag.textContent = "this is a button option 4";
+    // document.body.appendChild(tag);
+    // tag.className = 'questionsBtn';
+
+    // Code to assign the class questionsBtn to the appended buttons so the styles are applied
+
+
+    /* Code to create an if statement that will decrease the time counter in case of a wrong choice and creates an underline with the WRONG! text,
+    in case of correct choice display an underline and CORRECT! text, keeps the counter at the same time. The selection should pause the counter and 
+    also play a sound when its correct */
+}
+
+// function wrongChoice() {
+//     timerCount-15;
+// }
+// Code to redirect the user to a highscores HTML file that stores the data for the highscores
+
+// Code to create a submit form that will store the highscores results with a score of the time remaining.
+
+
+// function rightChoice();
 
 startBtn.addEventListener("click", startQuiz);
+// tag.addEventListener("click", wrongChoice);
+// tag.addEventListener("click", rightChoice);
+
+
+// tag.textContent = "Commonly used data types DO NOT include:";

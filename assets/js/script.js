f237f42f5dfeedd0def128852c12d43deb238435
AlexTrejo92 <arq.alextrejo@gmail.com>
2023-04-08 17:40:52 -0600

    Further improvements of code. WIP

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,47 @@
-console.log ('first Test');
+
+// List of variables that select different html elements
 
 var timerCounter = document.querySelector('.timer');
-
-var startBtn = document.querySelector(".startBtn");
-
+var startBtn = document.getElementById('startBtn');
+startBtn.addEventListener("click", startQuiz);
 var hideCont = document.querySelector('.content');
-
+var questionSection = document.getElementById('questionSection');
 var timer;
-
 var timerCount = 20;
-
-var firstquestion = 'h1';
-
-var questionOptions = 'button';
-
-var headerSelection = document.querySelectorAll("h1")
-
-var btnSelection = document.querySelectorAll('button')
-
+var headerSelection = document.querySelectorAll("h1");
 var questionIndex = 0;
 
+
+// This is an object that holds all of the Questions and possible answers for the quiz
 var questions = [
     {
-        questionText: "Question1",
-        options: ["red","blue","green"],
-        correctAnswer: "blue",
+        questionText: "Commonly used data types DO NOT include:",
+        options: ["1. strings","2. booleans","3. alerts", "4. numbers"],
+        correctAnswer: "3. alerts",
     },
     {
-        questionText: "Question2",
-        options: ["red","blue","green"],
-        correctAnswer: "blue",
+        questionText: "The condition in an if / else statement is enclosed within _______.",
+        options: ["1. quotes","2. curly brackets","3. parentheses", "4. square brackets"],
+        correctAnswer: "2. curly brackets",
     },
     {
-        questionText: "Question3",
-        options: ["red","blue","green"],
-        correctAnswer: "blue",
+        questionText: "Arrays in JavaScript can be used to store _______.",
+        options: ["1. numbers and strings","2. other arrays","3. booleans", "4. all of the above"],
+        correctAnswer: "4. all of the above",
     },
+    {
+        questionText: "String values must be enclosed within ____ when being assigned to variables.",
+        options: ["1. commas","2. curly brackets","3. quotes", "4. parentheses"],
+        correctAnswer: "3. quotes", 
+    }
 ]
 
+// This function initializes the counter and hides the main section so that the questions are displayed
 function startQuiz() {
     timerCount = 20;
     startTimer();
-    hideCont.setAttribute('style', 'display: none');
+    hideCont.classList.add('hidden');
+    questionSection.classList.remove("hidden");
     runGame();
 }
 
@@ -49,9 +49,6 @@ function startTimer() {
 timer = setInterval(function() {
     timerCount--;
     timerCounter.textContent = timerCount;
-    if (timerCount >=0) {
-        console.log('test1');
-    }
     if (timerCount ===0) {
         clearInterval(timer);
     }
@@ -60,18 +57,63 @@ timer = setInterval(function() {
 // When timer runs out, it should stop the quiz (or runGame function?) and show a sumbit highscore form.
 
 function runGame() {
+// This line adds content to the h2 tag on the HTML and changes it according to the questions inside the Object called 'Questions'
     var questionEl = document.getElementById("questionText");
-    console.log(questions[1].questionText);
     questionEl.textContent = questions[questionIndex].questionText
 
-    var qArray = questions[questionIndex];
+    var choices = document.getElementById("choices");
+// This loop creates 4 different buttons based on the choices key within the 'Questions' object.
+for (var i = 0; i < questions[questionIndex].options.length; i++) {
+    choices = document.createElement('button');
+    choices.textContent = questions[questionIndex].options[i];
+    document.body.appendChild(choices);
+    choices.className = 'choicesBtn';
+    }
 
-for (var i = 0; i < qArray.options.length; i++) {
-var tag = document.createElement(questionOptions);
-tag.textContent = qArray.options[i];
-document.body.appendChild(tag);
-tag.className = 'questionStyle';
+var choicesBtns = document.querySelectorAll(".choicesBtn")
+console.log(choicesBtns);
+console.log(typeof choicesBtns);
+
+choicesBtns.addEventListener("click", validation); 
+
+function validation() {
+    if (userSelection === questions[questionIndex].correctAnswer) {
+        console.log('correct!')
+    } else {
+        console.log('incorrect')
+    }
 }
+}
+
+function nextQuestion() {
+    console.log('this');
+    console.log('is');
+    console.log('a test');
+};
+
+// choices.addEventListener("click", validation);
+
+// }
+
+
+// function endGame() {
+//     console.log('THE GAME HAS ENDED');
+// }
+
+// if (timerCount ===0) {
+//     endGame();
+// }
+
+// function validation() {
+//     if 
+// }
+
+
+// if 
+
+// // tag.addEventListener("click", console.log('hello world'));
+
+// }
 
     // var tag = document.createElement(firstquestion);
     // tag.textContent = "This is a Question";
@@ -83,28 +125,13 @@ tag.className = 'questionStyle';
     // document.body.appendChild(tag);
     // tag.className = 'questionsBtn';
 
-    // var tag = document.createElement(questionOptions);
-    // tag.textContent = "this is a button option 2";
-    // document.body.appendChild(tag);
-    // tag.className = 'questionsBtn';
-
-    // var tag = document.createElement(questionOptions);
-    // tag.textContent = "this is a button option 3";
-    // document.body.appendChild(tag);
-    // tag.className = 'questionsBtn';
-
-    // var tag = document.createElement(questionOptions);
-    // tag.textContent = "this is a button option 4";
-    // document.body.appendChild(tag);
-    // tag.className = 'questionsBtn';
-
     // Code to assign the class questionsBtn to the appended buttons so the styles are applied
 
 
     /* Code to create an if statement that will decrease the time counter in case of a wrong choice and creates an underline with the WRONG! text,
     in case of correct choice display an underline and CORRECT! text, keeps the counter at the same time. The selection should pause the counter and 
     also play a sound when its correct */
-}
+
 
 // function wrongChoice() {
 //     timerCount-15;
@@ -116,9 +143,11 @@ tag.className = 'questionStyle';
 
 // function rightChoice();
 
-startBtn.addEventListener("click", startQuiz);
+
 // tag.addEventListener("click", wrongChoice);
 // tag.addEventListener("click", rightChoice);
 
 
 // tag.textContent = "Commonly used data types DO NOT include:";
+
+// console.log(choices)
